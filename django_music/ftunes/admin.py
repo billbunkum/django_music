480398dc35d9fdd7b6bdf8caf75048703e7656b7
billbunkum <william.richard.hurst@gmail.com>
2016-09-20 16:40:41 -0400

    finish models

--- django_music/ftunes/admin.py
+++ django_music/ftunes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Album, Track, Genre, Artist
 # Register your models here.
+
+class AlbumAdmin(admin.ModelAdmin):
+	search_fields = ('name',)
+
+admin.site.register(Album, Track, Genre, Artist, AlbumAdmin)
--- django_music/ftunes/models.py
+++ django_music/ftunes/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Album(model.Models):
-	name = models.CharField(max_length=200)
+	name = models.CharField(max_length=50)
 	artist = models.ForeignKey('Artist')
 	genre = models.ForeignKey('Genre')
 
@@ -10,20 +10,30 @@ class Album(model.Models):
 		return "{}, {}, {}".format(self.name, self.artist, self.genre)
 
 class Track(model.Models):
-	name = models.CharField(max_length=200)
+	name = models.CharField(max_length=50)
 	album = models.ForeignKey('Album')
 
 	def __str__(self):
 		return self.name
 
 class Artist(model.Models):
-	name = models.CharField(max_length=200)
+	name = models.CharField(max_length=50)
 
 	def __str__(self):
 		return self.name
 
 class Genre(model.Models):
-	name = models.CharField(max_length=200)
+	CATEGORY_CHOICES = (
+		('ALTERNATIVE', 'alternative'),
+		('DISCO', 'disco'),
+		('JAM_BAND', 'jam band'),
+		('ROCK', 'rock'),
+		('POP', 'pop'),
+		('BAD', 'bad'),
+		('GOOD', 'good'),
+	)
+
+	name = models.CharField(max_length=50, default=ALTERNATIVE, choices=CATEGORY_CHOICES)
 
 	def __str__(self):
 		return self.name

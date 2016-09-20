aefaebd60286f4fafa6706cc4b3d738004053cb1
billbunkum <william.richard.hurst@gmail.com>
2016-09-20 16:08:12 -0400

    add classes/models

--- django_music/ftunes/models.py
+++ django_music/ftunes/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Album(model.Models):
+	name = models.CharField(max_length=200)
+	artist = models.ForeignKey('Artist')
+	genre = models.ForeignKey('Genre')
+
+	def __str__(self):
+		return "{}, {}, {}".format(self.name, self.artist, self.genre)
+
+class Track(model.Models):
+	name = models.CharField(max_length=200)
+	album = models.ForeignKey('Album')
+
+	def __str__(self):
+		return self.name
+
+class Artist(model.Models):
+	name = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.name
+
+class Genre(model.Models):
+	name = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.name
